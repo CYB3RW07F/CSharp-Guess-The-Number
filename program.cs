1c70d1ac556c512199ece5de6362ea81b1db5216
CYB3RW07F <marcbaantjes@hotmail.com>
2025-07-05 19:33:53 +0200

    random stuff

---
+++ program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to the Sharp number guesser!");
+      int randomnumber = 0;
+      int guess = Convert.ToInt32(Console.ReadLine()); 
+      while (guess != randomnumber)
+      {
+
+      }
+    }
+  }
+}
